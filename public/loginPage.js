e003886b0e6498338978acc4aa0476208b82b92d
Dmitry Amachenko <damachenko@mail.ru>
2025-03-10 13:46:56 +0300

    initial commit

---
+++ public/loginPage.js
@@ -0,0 +1,33 @@
+"use strict"; // Включаем строгий режим
+
+class UserForm {
+    constructor() {
+        this.loginFormCallback = this.loginFormAction.bind(this);
+        this.registerFormCallback = this.registerFormAction.bind(this);
+    }
+
+    loginFormAction(data) {
+        ApiConnector.login(data, (response) => {
+            console.log(response); // Логируем ответ сервера
+            if (response.success) {
+                location.reload(); // Обновляем страницу при успешной авторизации
+            } else {
+                alert(`Ошибка авторизации: ${response.error}`); // Выводим ошибку
+            }
+        });
+    }
+
+    registerFormAction(data) {
+        ApiConnector.register(data, (response) => {
+            console.log(response); // Логируем ответ сервера
+            if (response.success) {
+                location.reload(); // Обновляем страницу при успешной регистрации
+            } else {
+                alert(`Ошибка регистрации: ${response.error}`); // Выводим ошибку
+            }
+        });
+    }
+}
+
+// Создаем объект класса UserForm
+const userForm = new UserForm();
